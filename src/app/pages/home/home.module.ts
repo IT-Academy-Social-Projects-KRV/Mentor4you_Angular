750cddd0cfd1bb664cb5499f5242b6695b8f6d81
Zhovtaniuk Maxym <zhovtyjshady@gmail.com>
2021-08-18 21:14:59 +0300

    home-page_slider_added

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
-
+import { IvyCarouselModule } from 'angular-responsive-carousel';
+import { CarouselComponent } from './components/carousel/carousel.component';
 
 @NgModule({
-  declarations: [HomeComponent],
-  imports: [HomeRoutingModule]
+  declarations: [HomeComponent, CarouselComponent],
+  imports: [HomeRoutingModule, IvyCarouselModule],
 })
-export class HomeModule { }
+export class HomeModule {}
