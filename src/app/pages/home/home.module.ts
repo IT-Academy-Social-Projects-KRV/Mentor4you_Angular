157da7d91700cd8708c17587c9129e699d9efab8
Zhovtaniuk Maxym <zhovtyjshady@gmail.com>
2021-08-20 20:06:14 +0300

    merged with dev

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
+
 import { IntroductionComponent } from './components/introduction/introduction.component';
 import { InvitationComponent } from './components/invitation/invitation.component';
 
+import { IvyCarouselModule } from 'angular-responsive-carousel';
+import { CarouselComponent } from './components/carousel/carousel.component';
 
 @NgModule({
-  declarations: [HomeComponent, IntroductionComponent, InvitationComponent],
-  imports: [HomeRoutingModule]
+  declarations: [HomeComponent, CarouselComponent, IntroductionComponent, InvitationComponent],
+  imports: [HomeRoutingModule, IvyCarouselModule],
 })
-export class HomeModule { }
+export class HomeModule {}
