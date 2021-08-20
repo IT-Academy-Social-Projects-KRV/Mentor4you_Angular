cb338bf65de43425bb07153e3d1305d344829e73
Zhovtaniuk Maxym <zhovtyjshady@gmail.com>
2021-08-20 20:11:38 +0300

    merged 4

--- src/app/pages/home/CELL.ts
+++ src/app/pages/home/CELL.ts
@@ -17,8 +17,8 @@ export const CELL: cell[] = [
   },
   {
     img: '../../assets/images/reviews-slider/0.svg',
-    title: '“Honestly awesome approach”',
-    text: 'Amazing content from google, they not only cover figma also they concentrated on UX and other design aspects, Very helpful to build by career. Thank you Coursera & Google.',
+    title: '“Interesting implementation”',
+    text: 'Amazing content from mentor4you, they not only cover figma also they concentrated on UX and other design aspects, Very helpful to build by career. Thank you Mentor4you!',
     name: 'Alisa rahman',
     mark: 5,
   },
