32521494ff2f9b8fedae341b385f6f2c937f0f06
Vitalii Ploshchynskyi <ploschinsskiyvv@gmail.com>
2021-08-24 14:37:32 +0300

    finished create stars raiting

---
+++ src/app/pages/mentor/components/mentor-add-review-section/mentor-add-review-section.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit, ViewChild, Input, Output, EventEmitter, AfterViewInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-mentor-add-review-section',
+  templateUrl: './mentor-add-review-section.component.html',
+  styleUrls: ['./mentor-add-review-section.component.scss']
+})
+export class MentorAddREviewSectionComponent implements OnInit {
+
+  btnCheck: boolean = false;
+  formAddReview!: FormGroup;
+  stars = [1, 2, 3, 4, 5];
+  rating = 0;
+  hoverState = 0;
+  constructor() { }
+
+  ngOnInit(): void { 
+     this.formAddReview = new FormGroup({
+      name: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      textarea: new FormControl('', Validators.required),
+    })
+  }
+  addFocus() { }
+
+  submit() {
+    console.log(this.formAddReview.get('name')?.value);
+    // console.log(this.formAddReview['controls'].name);
+    
+
+  }
+  btnClick() {
+    this.btnCheck = !this.btnCheck;
+  }
+  enter(i:number) {
+    this.hoverState = i;
+  }
+
+  leave() {
+    this.hoverState = 0;
+  }
+
+  updateRating(i:number) {
+    this.rating = i;
+    
+    console.log(this.rating)
+  }
+
+}
