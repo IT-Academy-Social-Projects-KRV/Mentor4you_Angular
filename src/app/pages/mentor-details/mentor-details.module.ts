94425c1311e0506436723aaf50ffb4876d963f40
Vitalii Ploshchynskyi <ploschinsskiyvv@gmail.com>
2021-08-24 21:26:04 +0300

    transfer component Iliy and Vitalii with mentors file to mentor-details

--- src/app/pages/mentor-details/mentor-details.module.ts
+++ src/app/pages/mentor-details/mentor-details.module.ts
@@ -1,15 +1,27 @@
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { MentorDetailsComponent } from './mentor-details.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { MentorDetailsRoutingModule } from './mentor-details-routing.module';
+import { MentorAddREviewSectionComponent } from './components/mentor-add-review-section/mentor-add-review-section.component';
+import { AppStarRaitingComponent } from '../../shared/sharedComponents/star-raiting/app-star-raiting';
+import { MainSectionComponent } from './components/main-section/main-section.component';
+
 
 
 @NgModule({
-  declarations: [MentorDetailsComponent],
+  declarations: [
+    MentorDetailsComponent,
+    MentorAddREviewSectionComponent,
+    AppStarRaitingComponent, 
+    MainSectionComponent],
+    
   imports: [
     SharedModule,
     MentorDetailsRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
   ]
 })
 export class MentorDetailsModule { }
