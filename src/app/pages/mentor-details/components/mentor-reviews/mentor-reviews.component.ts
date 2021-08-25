df503dd16226c0228be29c9110b42bb788218ba9
Andrii Rud <andrii.rud@gmail.com>
2021-08-25 12:37:41 +0300

    add mentor_reviews section

---
+++ src/app/pages/mentor-details/components/mentor-reviews/mentor-reviews.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mentor-reviews',
+  templateUrl: './mentor-reviews.component.html',
+  styleUrls: ['./mentor-reviews.component.scss']
+})
+export class MentorReviewsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
