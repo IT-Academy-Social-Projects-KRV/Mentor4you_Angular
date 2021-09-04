8cdfda724ec276f49a55ea46ca37c2136fbee401
danpuch7 <illia.demchishin@gmail.com>
2021-09-04 16:48:15 +0300

    create sigin_component

--- src/app/auth/signin/signin.module.ts
+++ src/app/auth/signin/signin.module.ts
@@ -1,21 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import {HttpClient, HttpClientModule} from '@angular/common/http'
+import {HttpClientModule} from '@angular/common/http'
 
 
 import { SigninRoutingModule } from './signin-routing.module';
 import {NgbDropdownModule} from "@ng-bootstrap/ng-bootstrap";
 import {SigninComponent} from "./signin.component";
+import {ReactiveFormsModule} from "@angular/forms";
 
 
 @NgModule({
-  declarations: [],
+  declarations: [SigninComponent],
   imports: [
     CommonModule,
     SigninRoutingModule,
     HttpClientModule,
     NgbDropdownModule,
+    ReactiveFormsModule,
   ]
 })
 export class SigninModule { }
