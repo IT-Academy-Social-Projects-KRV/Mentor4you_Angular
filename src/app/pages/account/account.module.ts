b57b6507442064263f2dffbf94be68359fd9994d
Oleh Ostrikov <oleg1ostrikov@gmail.com>
2021-09-13 22:25:15 +0300

    created section upload avatar for account page and part of account-mentor component

--- src/app/pages/account/account.module.ts
+++ src/app/pages/account/account.module.ts
@@ -1,22 +1,28 @@
 import { NgModule } from '@angular/core';
 
+import { MatChipsModule } from '@angular/material/chips';
+
 import { AccountRoutingModule } from './account-routing.module';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { AccountMentorComponent } from './components/account-mentor/account-mentor.component';
 import { AccountMenteeComponent } from './components/account-mentee/account-mentee.component';
 import { AccountSettingsComponent } from './components/account-settings/account-settings.component';
-
+import { AccountComponent } from './account.component';
+import { ChipsInputComponent } from './components/chips-input/chips-input.component';
 
 
 @NgModule({
   declarations: [
+    AccountComponent,
     AccountMentorComponent,
     AccountMenteeComponent,
-    AccountSettingsComponent
+    AccountSettingsComponent,
+    ChipsInputComponent
   ],
   imports: [
     SharedModule,
-    AccountRoutingModule
+    AccountRoutingModule,
+    MatChipsModule
   ]
 })
 export class AccountModule { }
--- src/app/pages/account/components/account-mentor/account-mentor.component.ts
+++ src/app/pages/account/components/account-mentor/account-mentor.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { Category } from '../chips-input/chips-input.component';
 
 @Component({
   selector: 'app-account-mentor',
@@ -6,10 +9,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./account-mentor.component.scss']
 })
 export class AccountMentorComponent implements OnInit {
+	@Input() mentorData?: {};
+	
+  isAccountActivated?: boolean;
+	gettedCategories: Category[] = [];
+  mentorForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder
+  ) {
+    this.mentorForm = this.fb.group({
+      'isAccountActivated': [false],
+      'fullName': ['', [
+        Validators.required,
+        Validators.pattern(/^[a-zA-Z]+$/),
+        Validators.minLength(3)
+      ]],
+      'about': ['', [Validators.required, Validators.minLength(5)]],
+      'categories': [[], Validators.required],
+      'email': ['', [Validators.required, Validators.email]]
+    })
+  }
 
   ngOnInit(): void {
   }
 
+  isControlInvalid(controlName: string): boolean {
+    const control = this.mentorForm.controls[controlName];
+    return control.invalid && control.touched;
+	}
+	
+	onSendCategories(categories: Category[]): void {
+
+	}
+
+  onSubmit(): void {
+
+  }
 }
