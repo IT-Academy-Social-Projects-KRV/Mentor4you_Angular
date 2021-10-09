9e350113387148b31a66fb4522e3ae89b20d0474
Oksana Adamchuk <adamchuk.oksana01.08@gmail.com>
2021-10-09 04:26:05 +0300

    added form to send email

--- src/app/pages/contacts/send-form/send-form.component.ts
+++ src/app/pages/contacts/send-form/send-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from "@angular/forms";
+import { FormBuilder, FormGroup, FormControl, Validators } from "@angular/forms";
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
@@ -9,10 +9,32 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class SendFormComponent implements OnInit {
   
-  constructor(private http: HttpClient){}  
+  sendingEmailForm: FormGroup;
+
+  constructor(private http: HttpClient, private formBuilder: FormBuilder){
+    this.sendingEmailForm = formBuilder.group({
+      fullNameControl: ['', Validators.required],
+      emailControl: ['', [Validators.required, Validators.email]],
+      subjectControl:['', [Validators.required, Validators.minLength(5)]],
+      messageControl: ['', [Validators.required, Validators.minLength(20)]],
+    }); 
+  }  
 
 
   ngOnInit() { }
+
+  sendData(){
+    console.log(this.sendingEmailForm.value.messageControl);
+    const reqBody = {
+      "emailAdrId": 0,
+      "emailAdres": this.sendingEmailForm.value.emailControl,
+      "name": this.sendingEmailForm.value.fullNameControl,
+      "subject": this.sendingEmailForm.value.subjectControl,
+      "message": this.sendingEmailForm.value.messageControl
+    }
+    console.log(reqBody);
+    this.http.post("http://localhost:8080/api/emailToModerator/sendEmailToModer", reqBody).subscribe(response => console.log(response))
+  }
 
 
 }
