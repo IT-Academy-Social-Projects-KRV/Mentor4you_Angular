50bffcf9ac232f4f63a1b9acff0a83c202c530d8
Vitalii Ploshchynskyi <ploschinsskiyvv@gmail.com>
2021-10-11 09:03:19 +0300

    create chat page

--- src/app/pages/messages/dialog-board/dialog-board.component.ts
+++ src/app/pages/messages/dialog-board/dialog-board.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgModel } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Chat } from 'src/app/core/services/chat.service';
+import {WebSocketService} from "../web-socket.service";
 
 @Component({
 	selector:'dialog-board',
@@ -11,27 +12,58 @@ import { Chat } from 'src/app/core/services/chat.service';
 
 export class DialogBoardComponent implements OnInit{
 	sendid:string="";
-	recivid:string=""; 
+	recivid:string="";
 	chatMessage:string='';
+	messageObj!:any;
+	messageObjClone!:any;
+	date!:any;
+	globalMessage!:any;
   constructor(
 	  private routerNavigate: ActivatedRoute,
 	  private chat: Chat,
+    private socketService:WebSocketService
 	  ){
 
   }
 	ngOnInit(){
+		
 	
+		console.log(this.messageObj)
+		console.log(this.sendid, this.recivid)
 		console.log(this.chatMessage);
 		this.routerNavigate.params.subscribe(e =>{
 			[this.sendid, this.recivid] = e.id.split('_');
-			console.log(this.sendid)
+			console.log(this.sendid, this.recivid)
 			this.chat.getMessage(this.sendid, this.recivid).subscribe(console.log)
-		} )	
+		} )
+		this.socketService.checkMsg(this.sendid, this.recivid).subscribe((response: any) => {
+			// this.messageObj = response;
+			this.socketService.newMessages.push(...response)
+			console.log('msgALL:', response)
+		})
+		this.socketService.checkMsg( this.recivid,this.sendid).subscribe((response: any) => {
+			// this.messageObjClone = response;
+			
+			this.socketService.newMessages.push(...response)
+			console.log('messageObjClone:', response)
+			this.date=response.timestamp;
+			console.log('stringTime: '+this.date)
+
+		})
+		
+		this.socketService.newMessages.sort((a, b) => a.timestamp - b.timestamp);
+		this.globalMessage = this.socketService.newMessages;
+		console.log(this.socketService.newMessages);
 	}
 	sendChatMessage(){
 		if (this.chatMessage.trim()){
+			this.socketService.lastSms$.next(this.chatMessage)
 			console.log(this.chatMessage)
+      this.socketService.sendMsg(this.chatMessage,this.sendid,this.recivid)
+
 		}
-		return false
+
 	}
+
+
 }
