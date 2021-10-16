beb68d952d11001a2dadf6f1d67f1ee4af132f74
Oleh Ostrikov <oleg1ostrikov@gmail.com>
2021-10-16 23:36:40 +0300

    Refactoring http requests for mentor

--- src/app/core/services/mentor.service.ts
+++ src/app/core/services/mentor.service.ts
@@ -4,12 +4,8 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-import { Category, Certificate, MentorCard, MentorProfile, City } from '../interfaces';
-import { 
-  categoriesData,
-  certificateList as certificates,
-  citiesData
-} from 'src/app/pages/account/components/account-mentor/data';
+import { Category, MentorCard, MentorProfile, City } from '../interfaces';
+import { categoriesData, citiesData } from 'src/app/pages/account/components/account-mentor/data';
 
 @Injectable({
   providedIn: 'root'
@@ -28,7 +24,7 @@ export class MentorService {
       .get<any>(this.mentorsBestRatingUrl)
       .pipe(map(mentors => {
         return mentors
-          // .filter((m: any) => m.firstName !== null)
+          .filter((m: any) => m.firstName !== null)
           .map((mentor: any) => {
             return {
               id: mentor.id,
@@ -44,95 +40,59 @@ export class MentorService {
   getMentorById(id: number): Observable<any> {
     return this.http
       .get<any>(this.mentorBaseUrl + `/${id}`)
-      .pipe(map((mentorById: any) => {
-        const mentor = mentorById.accountInfo;
-        const socialMap = mentorById.accountInfo.socialMap;
-        const currentRate = mentorById.categoriesList[0];
-        const categories = mentorById.categoriesList.map((category: Category) => category.categories.name);
-
-        return {
-          id: mentor.id,
-          email: mentor.email,
-          firstName: mentor.firstName,
-          lastName: mentor.lastName,
-          // avatar: this.currentAvatar,         // expecting a change in structure of the data
-          avatar: mentor.avatar,
-          phoneNumFirst: socialMap.PhoneNumFirst,
-          categoriesList: categories,
-          rate: currentRate.rate,
-          currency: currentRate.currency,
-          telegram: socialMap.Telegram,
-          skype: socialMap.Skype,
-          linkedIn: socialMap.LinkedIn,
-          gitHub: socialMap.GitHub,
-          certificates: mentorById.certificates,
-          place: mentor.place,
-          groupServ: mentorById.groupServ,
-          languages: mentorById.languages,
-          description: mentorById.description,
-          isAccountActivated: mentorById.showable_status,
-          cities: mentorById.cities,
-          rating: mentorById.rating,
-          online: mentorById.online,
-          offlineOut: mentorById.offlineOut,
-          offlineIn: mentorById.offlineIn
-        }
-
-      }))
+      .pipe(map((mentorById: any) => this.transformDataForClient(mentorById)));
   }
 
   getMentorDTO(): Observable<any> {
     return this.http
       .get<any>(this.mentorBaseUrl + '/getMentorDTO/')
-      .pipe(map((mentorDTO: any) => {
-
-        // console.log('mDTO - server', mentorDTO);
-
-        const mentor = mentorDTO.accountInfo;
-        const socialMap = mentorDTO.accountInfo.socialMap;
-        const currentRate = mentorDTO.categoriesList[0] || 5;
-        const categories = mentorDTO.categoriesList.map((category: Category) => category.categories.name);
-        const cities = mentorDTO.cities.map((city: City) => city.name);
-
-        return {
-          id: mentor.id,
-          email: mentor.email,
-          firstName: mentor.firstName,
-          lastName: mentor.lastName,
-          // avatar: this.currentAvatar,         // expecting a change in structure of the data
-          avatar: mentor.avatar,
-          phoneNumFirst: socialMap.PhoneNumFirst,
-          categoriesList: categories,
-          rate: currentRate.rate,
-          currency: currentRate.currency,
-          telegram: socialMap.Telegram,
-          skype: socialMap.Skype,
-          linkedIn: socialMap.LinkedIn,
-          gitHub: socialMap.GitHub,
-          certificates: mentorDTO.certificates,
-          place: mentor.place,
-          groupServ: mentorDTO.groupServ,
-          languages: mentorDTO.languages,
-          description: mentorDTO.description,
-          isAccountActivated: mentorDTO.showable_status,
-          cities: cities,
-          rating: mentorDTO.rating,
-          online: mentorDTO.online,
-          offlineOut: mentorDTO.offlineOut,
-          offlineIn: mentorDTO.offlineIn
-        }
-      }))
+      .pipe(map((mentorDTO: any) => this.transformDataForClient(mentorDTO)));
   }
 
   updateMentor(mentor: any): Observable<any> {
-    const updatedMentor = this.transformData(mentor);
-
+    const updatedMentor = this.transformDataForServer(mentor);
     return this.http.put<any>(this.mentorBaseUrl + '/UpdateMentor/', updatedMentor);
   }
 
-  transformData(mentor: any): Object {
 
-    // console.log('mentor - to Server', mentor);
+  // ---------- transform data for Frontend & Backend --------------
+  
+  transformDataForClient(mentor: any) {
+    const mentorData = mentor.accountInfo;
+    const socialMap = mentor.accountInfo.socialMap;
+    const currentRate = mentor.categoriesList[0] || 5;
+    const categories = mentor.categoriesList.map((category: Category) => category.categories.name);
+    const cities = mentor.cities.map((city: City) => city.name);
+
+    return {
+      id: mentorData.id,
+      email: mentorData.email,
+      firstName: mentorData.firstName,
+      lastName: mentorData.lastName,
+      avatar: mentorData.avatar,
+      phoneNumFirst: socialMap.PhoneNumFirst,
+      categoriesList: categories,
+      rate: currentRate.rate,
+      currency: currentRate.currency,
+      telegram: socialMap.Telegram,
+      skype: socialMap.Skype,
+      linkedIn: socialMap.LinkedIn,
+      gitHub: socialMap.GitHub,
+      certificates: mentor.certificates,
+      place: mentorData.place,
+      groupServ: mentor.groupServ,
+      languages: mentor.languages,
+      description: mentor.description,
+      isAccountActivated: mentor.showable_status,
+      cities: cities,
+      rating: mentor.rating,
+      online: mentor.online,
+      offlineOut: mentor.offlineOut,
+      offlineIn: mentor.offlineIn
+    }
+  }
+
+  transformDataForServer(mentor: any): Object {
 
     const newCategories = categoriesData.filter((category: Category) => {
       if (mentor.categoriesList.includes(category.categories.name)) {
@@ -146,8 +106,6 @@ export class MentorService {
     });
 
     const newSities = citiesData.filter(city => mentor.cities.includes(city.name));
-
-    // console.log('category', newCategories);
 
     return {
       accountInfo: {
