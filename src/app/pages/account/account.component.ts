6d961bb76c5f94aba781ad672f2c11843a38e46d
Oleh Ostrikov <oleg1ostrikov@gmail.com>
2021-11-04 19:43:43 +0200

    Refactoring mentor account

--- src/app/pages/account/account.component.ts
+++ src/app/pages/account/account.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ImageCroppedEvent, base64ToFile } from 'ngx-image-cropper';
-import { take } from 'rxjs/operators';
+import { of } from 'rxjs';
+import { catchError, take } from 'rxjs/operators';
 
 import { Certificate, MenteeService, MentorProfile, MentorService, UserService } from 'src/app/core';
 import { SigninService } from 'src/app/auth/signin/signin.service';
+import AdamEndEve from 'src/app/core/mock/AdamEndEve';
 
 
 @Component({
@@ -34,20 +35,18 @@ export class AccountComponent implements OnInit {
 
   constructor(
     public auth: SigninService,
-    private http: HttpClient, 
-    private _snackBar: MatSnackBar,
+    private snackBar: MatSnackBar,
     private mentorService: MentorService,
     private menteeService: MenteeService,
     private userService: UserService
   ) {}
   
-
-  get isAuth() {
-    return this.auth.isAuth();
+  ngOnInit(): void {
+    this.getUsers();
   }
 
-  ngOnInit(): void {
-    switch (localStorage.getItem('role')){
+  getUsers(): void {
+    switch (localStorage.getItem('role')) {
       case "MENTOR":  
         this.mentorService
           .getMentorDTO()
@@ -70,6 +69,10 @@ export class AccountComponent implements OnInit {
     }
   }
 
+  get isAuth(): boolean {
+    return this.auth.isAuth();
+  }
+
   setMentorData(mentorData: any): void {
     this.isAccountActivated = mentorData.isAccountActivated;
     this.imageCropped(mentorData.avatar);
@@ -88,7 +91,7 @@ export class AccountComponent implements OnInit {
     
     let ext = this.selectedFile.name.substring(this.selectedFile.name.lastIndexOf('.') + 1).toLowerCase();
     
-    let sizeInKB = Math.round(this.selectedFile.size/1024);
+    let sizeInKB = Math.round(this.selectedFile.size / 1024);
   
     if (ext != 'png' && ext != 'jpg' && ext != 'jpeg') {
       this.openSnackBar('Please select a photo', 'Got it', 'danger');
@@ -113,13 +116,13 @@ export class AccountComponent implements OnInit {
     
   }
    
-  imageCropped(event: ImageCroppedEvent) {
+  imageCropped(event: ImageCroppedEvent): void {
     this.croppedImage = event.base64;
     this.fileC = base64ToFile(this.croppedImage);
     this.myFile = new File([this.fileC], this.newName, {lastModified:  Date.now(), type:  this.imgType});
   }
 
-  imgReady() {
+  imgReady(): void {
     this.isImage = false;
     this.isBtnDisabled = false;
   }
@@ -134,41 +137,79 @@ export class AccountComponent implements OnInit {
     }
 
     fd.append('file', file);
+
+    //   this.userService.uploadAvatar(fd).pipe(take(1)).subscribe(
+    //     () => { },
+    //     () => {this.textFieldUpload = 'Something went wrong. Please, try again!'},
+    //     () => {
+    //       this.textFieldUpload = 'Your photo uploaded successfully!';
+    //       this.isBtnDisabled = true; 
+    //     }
+    // );
    
-    this.userService.uploadAvatar(fd).pipe(take(1)).subscribe(
-      () => { },
-      () => {this.textFieldUpload = 'Something went wrong. Please, try again!'},
-      () => {
-        this.textFieldUpload = 'Your photo uploaded successfully!';
-        this.isBtnDisabled = true; 
-      }
-  );
+    this.userService
+      .uploadAvatar(fd)
+      .pipe(
+        take(1),
+        catchError(() => this.textFieldUpload = 'Something went wrong. Please, try again!')
+        )
+      .subscribe(
+        () => { 
+          this.textFieldUpload = 'Your photo uploaded successfully!';
+          this.isBtnDisabled = true;
+        }
+      );
 
     this.userService.setAvatar(this.croppedImage);
-
-    this.appearanceAdamAndEve();       // ------------------ Adam & Eve -----------------------
+      
+    // ------------------ Adam & Eve -----------------------
+    if (this.croppedImage === AdamEndEve) {
+      this.appearanceAdamAndEve();
+    }
   }
 
-  appearanceAdamAndEve() {
+  appearanceAdamAndEve(): void {
     this.userService.setName('Adam & Eve');
   }
 
-  deleteAvatar(){
-    this.userService.deleteAvatar().pipe(take(1)).subscribe(response => console.log(response),
-    error => { if (error.status == 200) {
-        this._snackBar.open('Photo deleted! Now you have basic avatar', '', {
-          duration: 3000
-        });
-      } else {
-        this._snackBar.open('Error. Try again later', '', {
-          duration: 3000
-        });
-      }
-    });
+  // deleteAvatar(){
+  //   this.userService.deleteAvatar().pipe(take(1)).subscribe(response => console.log(response),
+  //   error => { if (error.status == 200) {
+  //       this.snackBar.open('Photo deleted! Now you have basic avatar', '', {
+  //         duration: 3000
+  //       });
+  //     } else {
+  //       this.snackBar.open('Error. Try again later', '', {
+  //         duration: 3000
+  //       });
+  //     }
+  //   });
+  // }
+
+  deleteAvatar(): void {
+    this.userService
+      .deleteAvatar()
+      .pipe(
+        take(1),
+        catchError(error => of(this.hendelError(error)))
+      )
+      .subscribe(response => console.log(response));
+  }
+
+  hendelError(error: any) {
+    if (error.status == 200) {
+      this.snackBar.open('Photo deleted! Now you have basic avatar', '', {
+        duration: 3000
+      });
+    } else {
+      this.snackBar.open('Error. Try again later', '', {
+        duration: 3000
+      });
+    }
   }
 
   openSnackBar(message: string, action: string, className: string) {
-    this._snackBar.open(message, action, {
+    this.snackBar.open(message, action, {
       duration: 5000,
       panelClass: className,
     });
